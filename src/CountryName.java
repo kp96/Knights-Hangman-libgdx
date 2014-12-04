1fb3e0180953b6836f451fba34120f0c10cfc28e
Krishna Kalubandi <krishnakalubandi@gmail.com>
2014-12-04 14:31:13 +0530

    Basic definitons

---
+++ src/CountryName.java
@@ -0,0 +1,15 @@
+
+public class CountryName {
+
+	String[] countries;
+	public String getCountryName()
+	{
+		//use random class :P or anything 
+		return "india";
+	}
+	public char getCharOfCountry(String country)
+	{
+		//your code here. make it so that if the country is india it should send each char only once
+		return 'c';
+	}
+}
