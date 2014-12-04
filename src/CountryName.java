69be2e88c8c2c1cdfb287970e5e6817b9465dbbc
Hemanth <hvaddi9@gmail.com>
2014-12-04 15:35:07 +0530

    updated countries

--- src/CountryName.java
+++ src/CountryName.java
@@ -5,9 +5,15 @@ public class CountryName {
 	public String getCountryName()
 	{
 		//use random class :P or anything 
-		//Hello this is hemanth
-		//hello kp here
-		return "india";
+		String[] list={"ITALY","INDIA","FRANCE","SPAIN","NIGERIA","EUCADOR","MADAGASCAR","SWITZERLAND","SWEDEN","INDONESIA",
+				       "CHINA","GREECE","AUSTRIA","JAMAICA","ARGENTINA","BAHAMAS","BAHARAIN","BRAZIL","COLOMBIA","ENGLAND",
+				       "CANADA","GRENADA","GUATEMALA","ETHIOPIA","HONDURAS","ISRAEL","CAMBODIA","GEORGIA","LIECHTENSTEIN","LUXEMBOURG",
+				       "MONTENEGRO","MARITANIA","LEBANON","KYRGYSTAN","KAZAKHSTAN","MACEDONIA","NAMIBIA","MICRONESIA","NICARAGUA","PHILIPPINES",
+				       "PORTUGAL","PARAGUAY","SEYCHELLES","ROMANIA","PALAU","PANAMA","PAKISTAN","NORWAY","POLAND","RUSSIA",
+				       "TANZANIA","THAILAND","UGANDA","UZBEKISTAN","VENEZULA","ZIMBABWE","TOGO","TUNISIA","VIETNAM","SENEGAL",
+				       "SCOTLAND","SLOVENIA","SLOVEKIA","DENMARK"};
+		int x=(int)Math.random()*(list.length);
+		return list[x];
 	}
 	public char getCharOfCountry(String country)
 	{
